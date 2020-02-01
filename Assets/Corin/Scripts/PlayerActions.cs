b189b9258c023c91494d42de7a554d628b1c55c7
corindonders <corindonders@gmail.com>
2020-02-01 11:40:04 +0100

    Poop

---
+++ Assets/Corin/Scripts/PlayerActions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerActions : MonoBehaviour
+{
+    public GameObject Ass;
+    public GameObject Poo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")){
+            Instantiate(Poo, Ass.transform.position, Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))));
+            //Debug.Log("poop");
+        }
+    }
+}
---
+++ Assets/Corin/Scripts/Quest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject VFX;
+    public GameObject UI;
+    private GameObject ScoreController;
+
+    //public string Name;
+    //public string Action;
+    public enum Actions{ ShitOn, Move, Breaks }
+    public Actions actions;
+    //public string description
+    public int scoreValue;
+    public bool completed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreController = GameObject.Find("@ Score");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuestSucces(){
+        if (completed == false) {
+        completed = true;
+        
+        VFX.SetActive(true);
+        UI.SetActive(false);
+        
+        ScoreController.GetComponent<Score>().AddScore (scoreValue);
+
+        }
+    }
+}
